fba2ae277e1372de3b1fca1098716ff1219c4a34
Mishaal khan <mishaalkhan135@gmail.com>
2024-12-26 21:22:59 +0500

    Added the auth flow

---
+++ src/lib/theme/foundations/breakpoints.tsx
@@ -0,0 +1,16 @@
+export const breakpoints = {
+	sm: "320px",
+	mm: "375px",
+	ml: "425px",
+	md: "768px",
+	lg: "1024px",
+	xl: "1440px",
+	"2xl": "1700px",
+};
+
+export const colors = {
+	secondaryGray: "rgba(106, 106, 106, 1)",
+	lightBg: "rgba(239, 239, 239, 1)",
+	cyberNutBlue: "#004DCD",
+	grayBlue: "rgba(70, 72, 86, 1)",
+};
---
+++ src/lib/theme/foundations/theme.tsx
@@ -0,0 +1,4 @@
+import { extendTheme } from "@chakra-ui/react";
+import { breakpoints, colors } from "./breakpoints";
+
+export default extendTheme({ breakpoints, colors });
